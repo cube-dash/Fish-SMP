68a06a9f1a88d9273418234adf8b86af1d557ea4
cube dash <ethanowl0@gmail.com>
2026-05-22 16:13:01 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot is running');
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log('🌐 Web port opened');
+});
 const mineflayer = require('mineflayer');
 const config = require('./config.json');
 
